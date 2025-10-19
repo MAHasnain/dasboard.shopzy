81b775ece19d383ac2f814f224f9c318deeffc2d
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2025-10-20 00:00:50 +0500

    feat: created dashboard login and register pages with complete HTML structure and JS logic integration

--- JS/login.js
+++ JS/login.js
@@ -1,1 +1,24 @@
-console.log(supabase);
+import { signInUser } from "../Database/auth.database.js";
+// console.log(supabase);
+
+const loginEmailInp = document.querySelector("#loginEmailInp");
+const loginPassInp = document.querySelector("#loginPassInp");
+const loginBtn = document.querySelector("#loginBtn");
+
+loginBtn.addEventListener("click", async (e) => {
+    e.preventDefault();
+    try {
+
+        const signedInUser = await signInUser({
+            email: loginEmailInp.value,
+            password: loginPassInp.value,
+        })
+
+        console.log(signedInUser);
+    
+    } catch (error) {
+        console.error(error)
+        return error;
+    }
+
+})
--- JS/register.js
+++ JS/register.js
@@ -1,1 +1,35 @@
-console.log(supabase);
+// console.log(supabase);
+import { createNewUser } from "../Database/auth.database.js";
+
+const regNameInp = document.querySelector("#regNameInp")
+const regAgeInp = document.querySelector("#regAgeInp")
+const regEmailInp = document.querySelector("#regEmailInp")
+const regPasswordInp = document.querySelector("#regPasswordInp")
+const registerBtn = document.querySelector("#register-btn")
+
+registerBtn.addEventListener("click", async (e) => {
+    e.preventDefault();
+    try {
+
+        const newUser = await createNewUser(
+            {
+                email: regEmailInp.value,
+                password: regPasswordInp.value,
+                options: {
+                    data: {
+                        first_name: regNameInp.value,
+                        age: regAgeInp.value,
+                    }
+                }
+            }
+        );
+
+        console.log(newUser);
+        return newUser;
+
+    } catch (error) {
+        console.error(error);
+        return error;
+    }
+
+})
