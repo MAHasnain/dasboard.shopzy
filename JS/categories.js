edd35b679f2b4e8c1a6bd71e5458205927dba820
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2025-10-21 09:44:49 +0500

    feat: added category creation form with basic HTML structure and JS logic

--- JS/categories.js
+++ JS/categories.js
@@ -1,1 +1,24 @@
-console.log(supabase);
+// console.log(supabase);
+import { createCategory } from "../Database/allMethods.js";
+
+const categoryCreationForm = document.querySelector("#categoryCreationForm");
+const titleInp = document.querySelector("#title-Inp");
+
+categoryCreationForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    try {
+
+        const newCategory = await createCategory(
+            {
+                title: titleInp.value
+            }
+        );
+
+        console.log(newCategory);
+
+    } catch (error) {
+        console.error(error);
+    }
+
+})
